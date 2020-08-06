6eadfcd19447afff2cce986dbb83eee7223ea9a6
user-1 <user-1@MacBook-Pro.local>
2020-08-06 16:53:08 -0600

    working on it

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class ClassList extends Component {
   constructor() {
     super()
-    
+    this.state = {
+      students = []
+    }
+  }
+  
+  componentDidMount(){
+     this.state.params.match.id()
   }
 
   render() {
+    const mappedStudents = this.state.students.map(student => {
+      return <h3 key={student.id}>{student.first_name} {student.last_name}</h3>
+    })
     return (
       <div className="box">
         <h1></h1>
         <h2>ClassList:</h2>
-
+        {mappedStudents}
       </div>
     )
   }
